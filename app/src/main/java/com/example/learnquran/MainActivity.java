b9289619ad24eabe753517b861c5d56afd80e222
Hiba Saqib <hibasaqib72@gmail.com>
2021-11-07 14:33:57 +0500

    Image and button to go to repository added

--- app/src/main/java/com/example/learnquran/MainActivity.java
+++ app/src/main/java/com/example/learnquran/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.learnquran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,15 @@ public class MainActivity extends AppCompatActivity {
         TextView txtRoll=findViewById(R.id.textView4);
         txtName.setText("Hiba Saqib");
         txtRoll.setText("BCSF18A540");
-
+        Button btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri web= Uri.parse("https://github.com/Hiba72/MC-Task03-Learn-Quran");
+                intent=new Intent(Intent.ACTION_VIEW,web);
+                startActivity(intent);
+            }
+        });
 
     }
 }
