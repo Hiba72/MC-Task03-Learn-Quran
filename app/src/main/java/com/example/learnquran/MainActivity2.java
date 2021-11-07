99b57e3e945da53966cf26dff6841faaf176bcb7
Hiba Saqib <hibasaqib72@gmail.com>
2021-11-07 17:21:42 +0500

    random arabic letter successfully generated

--- app/src/main/java/com/example/learnquran/MainActivity2.java
+++ app/src/main/java/com/example/learnquran/MainActivity2.java
@@ -2,13 +2,36 @@ package com.example.learnquran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    int rand,a,b;
+    Character c='\u0627';
+    Character c1='\u0649';
+    Character letter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Random random = new Random();
+        TextView txt = findViewById(R.id.textView5);
+        Button btnGenerate= findViewById(R.id.button4);
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a=Character.valueOf(c);
+                b=Character.valueOf(c1);
+                rand=random.nextInt(23)+a;
+                letter=(char) rand;
+                txt.setText(String.valueOf(letter));
+            }
+        });
     }
 }
