989e59dc01933347930a9041f8d51ed6a6adc01d
Hiba Saqib <hibasaqib72@gmail.com>
2021-11-07 14:57:01 +0500

    Second activity added

--- app/src/main/java/com/example/learnquran/MainActivity.java
+++ app/src/main/java/com/example/learnquran/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Intent intent;
+    Intent intent1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +24,20 @@ public class MainActivity extends AppCompatActivity {
         txtName.setText("Hiba Saqib");
         txtRoll.setText("BCSF18A540");
         Button btn=findViewById(R.id.button);
+        Button btn1=findViewById(R.id.button2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Uri web= Uri.parse("https://github.com/Hiba72/MC-Task03-Learn-Quran");
                 intent=new Intent(Intent.ACTION_VIEW,web);
                 startActivity(intent);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              intent1=new Intent(MainActivity.this,MainActivity2.class);
+              startActivity(intent1);
             }
         });
 
